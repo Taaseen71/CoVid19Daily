1c6e6cef17730da96ccaf774827ca886423d6b6c
Saadat Taaseen <taaseen.homework@gmail.com>
2020-04-27 00:15:06 -0400

    rendered homepage

--- covid/src/App.js
+++ covid/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import './App.css';
 import axios from 'axios';
 import { BrowserRouter as Saadat, Route, Switch } from 'react-router-dom';
-
+import data from './Components/data.json';
+import Home from './Components/Home';
 
 
 
@@ -9,15 +11,71 @@ import { BrowserRouter as Saadat, Route, Switch } from 'react-router-dom';
 
 function App() {
 
+    useEffect(() => {
+        // fetchData();
+        localData();
+    }, []);
 
 
+    const [global, setGlobal] = useState([]);
+    const [todaysDate, setTodaysDate] = useState();
+    const [country, setCountry] = useState([]);
+
+    //* THIS FETCHES THE API FROM APITRACKER CURRENTLY INACTIVE, WHILE PROGRAMMING
+    const fetchData = async () => {
+        const data = await axios('https://api.covid19api.com/summary');
+        const myData = data.data;
+
+        //* set Global Data
+        console.log(myData);
+        setGlobal(myData.Global);
+
+        //* Set data Date
+        let Date = myData.Date;
+        console.log(fixDate(Date));
+        setTodaysDate(fixDate(Date));
+
+        //* Set Country Details
+        setCountry(myData.Countries);
+        console.log(myData.Countries);
+    };
+
+    //* THIS IS LOCAL JSON FILE USED TO MAKE ALL MY DATA
+    const localData = () => {
+        //* Set Global Data
+        setGlobal(data[0].Global);
+        console.log('GlobalData=', data[0].Global);
+
+        //* Set data Date
+        let Date = data[0].Date;
+        console.log('Fixed Date= ', fixDate(Date));
+        setTodaysDate(fixDate(Date));
+
+        //* Set Country Details
+        setCountry(data[0].Countries);
+        console.log(data[0].Countries);
+    };
 
 
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+    //* THIS FUNCTION FIXES THE DATE THAT COMES COMPLETELY UNREADABLE
+    const fixDate = (date) => {
+        let full;
+        let year = date.slice(0, 4);
+        let month = date.slice(5, 7);
+        let day = date.slice(8, 10);
+        let time = date.slice(12, 19);
+        full = ` ${day}-${month}-${year}, Time:${time}`;
+        return full;
+    };
+
+
+    return (
+        <Saadat>
+            <div className="App">
+                <Home key={global} globalData={global} todaysDate={todaysDate} />
+            </div>
+        </Saadat>
+    );
 }
 
 export default App;
---
+++ covid/src/Components/Home.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+import data from './data.json';
+
+function Home(props) {
+  useEffect(() => {
+    console.log(props.globalData.NewConfirmed);
+  }, []);
+
+  return (
+    <div className='Home'>
+      <h1>{`Today's Date: ${props.todaysDate}`}</h1>
+      <h1>{`New Confirmed: ${props.globalData.NewConfirmed}`}</h1>
+      <h1>{`New Deaths: ${props.globalData.NewDeaths}`}</h1>
+      <h1>{`New Recovered: ${props.globalData.NewRecovered}`}</h1>
+      <h1>{`Total Confirmed Cases: ${props.globalData.TotalConfirmed}`}</h1>
+      <h1>{`Total Recovered Cases: ${props.globalData.TotalRecovered}`}</h1>
+    </div>
+  );
+}
+
+export default Home;
+
+
+
+//! Old fixDate method. use only if the other one doesnt work
+
+// const fixDate = (date) => {
+//   let Array= [];
+//   let returnStatement = []
+//   let firstSplit= date.match(/(.{1,10})/g);
+//   Array.push(firstSplit[0])
+//   Array.push(firstSplit[1]);
+//   let secondSplit = Array[1].match(/(.{1,9})/g)
+
+//   returnStatement.push(Array[0]);
+//   returnStatement.push(secondSplit[0]);
+
+//   let finalResult = returnStatement.join(', ')
+
+//   return finalResult;
+// }
+
+//! this method was was for testing. works fine
+
+//   useEffect (() => {
+//     // fetchData();
+//     localData();
+
+//   },[])
+
+//   const [global, setGlobal] = useState([]);
+//   const [todaysDate, setTodaysDate] = useState()
+
+//   const fetchData = async () => {
+//     const data= await fetch('https://api.covid19api.com/summary');
+
+//     const myData = await data.json();
+//     console.log(myData);
+//     setGlobal(myData.Global);
+//   }
+
+//   const localData = () => {
+//     setGlobal(data[0].Global);
+//     console.log(data[0].Date);
+//     let Date = data[0].Date;
+//     console.log(fixDate(Date));
+//     setTodaysDate(fixDate(Date));
+
+//   }
+
+//  const fixDate = (date) => {
+
+//    let full;
+//     let year= date.slice(0,4)
+
+//     let month = date.slice(5,7)
+//     let day= date.slice(8,10)
+//     let time = date.slice(12,19)
+//     full= ` ${day}-${month}-${year}, Time:${time}`
+
+//     return full;
+//   }
+
+//   return (
+//     <div className="Home">
+//       <h1>{`Today's Date: ${todaysDate}`}</h1>
+//     <h1>{`New Confirmed: ${global.NewConfirmed}`}</h1>
+//     <h1>{`New Deaths: ${global.NewDeaths}`}</h1>
+//     <h1>{`New Recovered: ${global.NewRecovered}`}</h1>
+//     <h1>{`Total Confirmed Cases: ${global.TotalConfirmed}`}</h1>
+//     <h1>{`Total Recovered Cases: ${global.TotalRecovered}`}</h1>
+//     </div>
+
+// );
