291f49496a765ce1279bf457d1a28be2fe7fd36a
Saadat Taaseen <taaseen.homework@gmail.com>
2020-04-26 23:55:23 -0400

    first commit

---
+++ covid/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Saadat, Route, Switch } from 'react-router-dom';
+
+
+
+
+
+
+function App() {
+
+
+
+
+
+  return (
+    <div className="App">
+      Hello
+    </div>
+  );
+}
+
+export default App;
