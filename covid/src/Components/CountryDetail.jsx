ed227f626b5be697837752c5ff66fd1555014a16
Saadat Taaseen <taaseen.homework@gmail.com>
2020-04-30 13:00:51 -0400

    fixed most bugs with Soleil

--- covid/src/Components/CountryDetail.jsx
+++ covid/src/Components/CountryDetail.jsx
@@ -1,17 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import data from './data.json';
 import '../App.css';
-import { useParams } from "react-router-dom"
+import { useParams, Redirect } from "react-router-dom";
+import { Bar } from "react-chartjs-2";
+
 
 function CountryDetail(props) {
-    const { id } = useParams();
+    console.log(props)
+
+    const [id, setId] = useState(useParams());
+    console.log(id.id)
+
+    const [newGraph, setNewGraph] = useState([]);
+    const [totalGraph, setTotalGraph] = useState([])
+    const [myDetail, setDetail] = useState([])
+
+
+    useEffect(() => {
+        // setDetail(myDetail)
+
+        setDetail(props.countryDetail.find(countryDetail => countryDetail.CountryCode === id.id))
+
+    }, [props])
+
+    console.log(myDetail)
+
+    useEffect(() => {
+        console.log(myDetail)
+        if (myDetail.Country) {
+            console.log('here')
+            createBarGraph1(myDetail);
+            createBarGraph2(myDetail);
+        }
+    }, [myDetail])
+
+    //! PROTECT WORKING CODE STARTING HERE
+
     console.log("id = ", id)
 
 
-    const detail = props.countryDetail.find(countryDetail => countryDetail.CountryCode === id)
-    console.log(detail)
 
-    if (!detail) return (<h1>Loading...</h1>);	//*Guard Operator works with && 
+
+    if (!props.countryDetail.length) return (<Redirect to="/" />);	//*Guard Operator works with && 
 
     const fixDate = (date) => {
         let full;
@@ -22,20 +52,112 @@ function CountryDetail(props) {
         full = ` ${month}/${day}/${year}, Time Updated: ${time}`;
         return full;
     };
+    //! ENDING HERE
+
+    const createBarGraph1 = (myDetail) => {
+        const myChart = {
+            chartData: {
+                labels: ['New Cases', 'New Recovered', 'New Deaths'],
+                datasets: [
+                    {
+                        label: 'Bar Graph',
+                        data: [
+                            myDetail.NewConfirmed,
+                            myDetail.NewConfirmed,
+                            myDetail.NewDeaths
+                        ],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(255, 206, 86, 0.6)'
+
+                        ],
+
+                    }
+                ]
+            }
+        };
+        console.log(myChart)
+        return setNewGraph(myChart)
+    }
+
+    const createBarGraph2 = (myDetail) => {
+        const myChart = {
+            chartData: {
+                labels: ['Total Cases', 'Total Recovered', 'Total Deaths'],
+                datasets: [
+                    {
+                        label: 'Bar Graph',
+                        data: [
+                            myDetail.TotalConfirmed,
+                            myDetail.TotalConfirmed,
+                            myDetail.TotalDeaths
+                        ],
+                        backgroundColor: [
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(153, 102, 255, 0.6)',
+                            'rgba(255, 159, 64, 0.6)',
+                            'rgba(255, 99, 132, 0.6)'
+                        ],
+
+                    }
+                ]
+            }
+        };
+
+        return setTotalGraph(myChart)
+    }
+
+
+
+    //TODO: WHERE TO CALL FUNCTION createaBarGraph1(myDetail)??
+    console.log(myDetail.Date)
 
     return (
         <>
             <div className="singleCountryDetail">
-                <h2>{fixDate(detail.Date)}</h2>
+                {myDetail.Date && <h2>{fixDate(myDetail.Date)}</h2>}
                 {/* <h2>{id}</h2> */} {/* Shows Params CountryCode */}
-                <img src={`https://www.countryflags.io/${id}/flat/64.png`}></img>
-                <h2>{detail.Country}</h2>
-                <h4>{`New Confirmed: ${detail.NewConfirmed}`}</h4>
-                <h4>{`New Deaths: ${detail.NewDeaths}`}</h4>
-                <h4>{`New Recovered: ${detail.NewRecovered}`}</h4>
-                <h4>{`Total Confirmed: ${detail.TotalConfirmed}`}</h4>
-                <h4>{`Total Recovered: ${detail.TotalRecovered}`}</h4>
-                <h4>{`Total Deaths: ${detail.TotalDeaths}`}</h4>
+                <img src={`https://www.countryflags.io/${id.id}/flat/64.png`}></img>
+                <h2>{myDetail.Country}</h2>
+                <h4>{`New Confirmed: ${myDetail.NewConfirmed}`}</h4>
+                <h4>{`New Deaths: ${myDetail.NewDeaths}`}</h4>
+                <h4>{`New Recovered: ${myDetail.NewRecovered}`}</h4>
+                <h4>{`Total Confirmed: ${myDetail.TotalConfirmed}`}</h4>
+                <h4>{`Total Recovered: ${myDetail.TotalRecovered}`}</h4>
+                <h4>{`Total Deaths: ${myDetail.TotalDeaths}`}</h4>
+            </div>
+            <div className="chart1">
+                <Bar
+                    data={newGraph.chartData}
+                    height="50px"
+                    options={{
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        },
+                        maintainAspectRatio: true
+                    }}
+                />
+            </div>
+            <div className="chart2">
+                <Bar
+                    data={totalGraph.chartData}
+                    height="50px"
+                    options={{
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        },
+                        maintainAspectRatio: true
+                    }}
+                />
             </div>
         </>
     );
--- covid/src/Components/Home.jsx
+++ covid/src/Components/Home.jsx
@@ -10,6 +10,8 @@ function Home(props) {
         createBarGraph2(props);
     }, []);
 
+    //* Graphs Start here
+
     const [newGraph1, setNewGraph1] = useState([])
     const [totalGraph1, setTotalGraph1] = useState([])
 
@@ -29,11 +31,8 @@ function Home(props) {
                         backgroundColor: [
                             'rgba(255, 99, 132, 0.6)',
                             'rgba(54, 162, 235, 0.6)',
-                            'rgba(255, 206, 86, 0.6)',
-                            'rgba(75, 192, 192, 0.6)',
-                            'rgba(153, 102, 255, 0.6)',
-                            'rgba(255, 159, 64, 0.6)',
-                            'rgba(255, 99, 132, 0.6)'
+                            'rgba(255, 206, 86, 0.6)'
+
                         ],
 
                     }
